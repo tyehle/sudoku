e08bc2497c37069ed4effc425fbe4a9d97713b68
Tobin Yehle <tobinyehle@gmail.com>
2016-08-24 16:30:04 -0600

    add a hard puzzle

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import System.IO
 
 main :: IO ()
 main = do
-  handle <- openFile "board-3x2.txt" ReadMode
+  handle <- openFile "board-3x3-hard.txt" ReadMode
   input <- hGetContents handle
   putStrLn $ case parseBoard input of
     Left err -> show err
