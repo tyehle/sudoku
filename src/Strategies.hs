866f1d42bcf500ecad401aebaa0bf649a89bcf35
Tobin Yehle <tobinyehle@gmail.com>
2016-08-29 14:46:57 -0600

    fix strategy execution order

--- src/Strategies.hs
+++ src/Strategies.hs
@@ -6,9 +6,21 @@ import Strategies.Internal
 import Board
 
 
-strats :: [Board -> Board]
-strats = [opOnGroups removeSolved, opOnGroups fixSingles]
-
 constrain :: Board -> Board
-constrain board = if board == updated then board else constrain updated
-  where updated = foldr1 (.) strats $ board
+constrain = execStrats [opOnGroups removeSolved, opOnGroups fixSingles]
+
+
+
+execStrats :: [Board -> Board] -> Board -> Board
+execStrats strats input = case bindAll input (map failWhenChanged strats) of
+  Left changed -> execStrats strats changed
+  Right done -> done
+
+bindAll :: (Foldable t, Monad m) => a -> t (a -> m a) -> m a
+bindAll start cs = foldr (=<<) (return start) cs
+
+failWhenChanged :: (Board -> Board) -> Board -> Either Board Board
+failWhenChanged fn input | changed == input = Right input
+                         | otherwise        = Left changed
+  where
+    changed = fn input
